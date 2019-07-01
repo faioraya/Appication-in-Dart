e83f76dc726640a35695793b310b54f912aaf7bd
faiory <jangyifao23@gmail.com>
2019-07-01 14:00:48 +0700

    Create Authen

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:faiory/screen/authen.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Authen(),
+      
+    );
+  }
+}
